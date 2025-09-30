67f5c6bb0a8e7cc8066950425c34adf7fb09c0e4
tinero28 <teru6525@mail.ru>
2025-10-01 01:50:40 +0300

    Add files via upload

---
+++ n6.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, count = 0, countCh = 0;
+    string r = "да";
+    cout << "введите число ";
+    cin >> n;
+    while (r != "нет") {
+        if (n % 2 == 0) countCh++;
+        count++;
+        cout << "если хотите прекратить ввод чисел, введите \"нет\": ";
+        cin >> r;
+        if (r != "нет") {
+            cout << "введите число ";
+            cin >> n;
+        }
+    }
+    cout << "всего введено чисел: " << count << "\nчетных: " << countCh;
+
+}
---
+++ n8.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i{ 0 }; i < n; i++) { // цикл, реализовывающий вывод n строк 
+
+		if (i % 2 == 0) for (int m{ 0 }; m < n; m += 2) cout << "  * "; // циклы, реализовывающие вывод n элементов 
+
+		else for (int m{ 0 }; m < n; m += 2) cout << "*   "; // за счет "if"/"else" реализована смена символов в начале строки 
+		cout << endl;
+	}
+}
